abca99c23f53e7fc592acd1e97667fe180c5822a
nobuohoribe01-pixel <nobuo.horibe01@gmail.com>
2025-11-02 01:26:05 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, send_file
+import segno
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route('/api/microqr')
+def generate_micro_qr():
+    data_to_encode = request.args.get('data', '')
+    if not data_to_encode:
+        return "Data parameter is missing.", 400
+    try:
+        qrcode = segno.make(data_to_encode, micro=True, error='m')
+        buffer = BytesIO()
+        qrcode.save(buffer, kind='png', scale=10, border=1)
+        buffer.seek(0)
+        return send_file(buffer, mimetype='image/png')
+    except Exception as e:
+        return f"Error: {e}", 500
